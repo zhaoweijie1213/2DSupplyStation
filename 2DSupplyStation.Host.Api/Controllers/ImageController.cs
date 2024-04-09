90bf7847b0bc8cbba06848cf44431c460f5a0253
zhaoweijie <949210784@qq.com>
2024-04-09 15:48:14 +0800

    添加API项目

---
+++ 2DSupplyStation.Host.Api/Controllers/ImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2DSupplyStation.Host.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+
+    }
+}
