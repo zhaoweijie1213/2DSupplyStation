0dd8ea1e3d9e858c8e870b52d54a725b91455519
zhaoweijie <949210784@qq.com>
2024-04-09 15:49:30 +0800

    修改swagger环境

--- 2DSupplyStation.Host.Api/Program.cs
+++ 2DSupplyStation.Host.Api/Program.cs
@@ -14,11 +14,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 
 app.UseAuthorization();
