d902e4e74caba0c44bcd76eacb9bb337cd239001
zhaoweijie <949210784@qq.com>
2024-04-11 14:57:43 +0800

    修改日志打印

--- 2DSupplyStation/Pages/Index.cshtml.cs
+++ 2DSupplyStation/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json;
+using System.Net;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace _2DSupplyStation.Pages
@@ -23,62 +24,72 @@ namespace _2DSupplyStation.Pages
 
         public void OnGet(string product = "Normal")
         {
+            string ipAddress = "";
+            // 获取IP地址
+            if (HttpContext.Request.Headers.TryGetValue("X-Forwarded-For", out Microsoft.Extensions.Primitives.StringValues value))
+            {
+                ipAddress = value.ToString();
+            }
+            else
+            {
+                IPAddress? remoteIpAddress = HttpContext.Connection.RemoteIpAddress;
+                if (remoteIpAddress != null)
+                {
+                    ipAddress = remoteIpAddress.ToString();
+                }
+
+            }
+            // 获取User Agent
+            string userAgent = HttpContext.Request.Headers["User-Agent"].ToString();
+
+            // 使用获取的信息
+            // 例如，将它们返回在视图中或进行某些处理
+
+            _logger.LogInformation("IP Address: {ipAddress}, User Agent: {userAgent}", ipAddress, userAgent);
 
             Images = GetImages(product);
 
-            //var imagesDir = Path.Combine(_environment.WebRootPath, "images", product);
 
-            //_logger.LogInformation("OnGet.图片路径:{imagesDir}", imagesDir);
-
-            //if (Directory.Exists(imagesDir))
-            //{
-
-            //    Images = Directory.EnumerateFiles(imagesDir)
-            //                      .Where(file => sourceArray.Contains(Path.GetExtension(file)?.ToLower()))
-            //                      .Select(filePath => new ImageInfo
-            //                      {
-            //                          FileName = Path.GetFileNameWithoutExtension(filePath),
-            //                          FilePath = $"/images/{product}/" + Path.GetFileName(filePath)
-            //                      })
-            //                      .OrderBy(i => i.FileName)
-            //                      .ToList();
-
-            //    _logger.LogInformation("OnGet.图片列表:{images}", JsonConvert.SerializeObject(Images));
-            //}
-            //else
-            //{
-            //    Images = new List<ImageInfo>();
-            //}
         }
 
         public List<ImageInfo> GetImages(string product)
         {
-            string key = $"data_{product}";
-            if (!memoryCache.TryGetValue(key,out List<ImageInfo>? images))
+            try
             {
-                var imagesDir = Path.Combine(_environment.WebRootPath, "images", product);
-
-                _logger.LogInformation("OnGet.图片路径:{imagesDir}", imagesDir);
-
-                if (Directory.Exists(imagesDir))
+                string key = $"data_{product}";
+                if (!memoryCache.TryGetValue(key, out List<ImageInfo>? images))
                 {
+                    var imagesDir = Path.Combine(_environment.WebRootPath, "images", product);
 
-                    images = Directory.EnumerateFiles(imagesDir)
-                                      .Where(file => sourceArray.Contains(Path.GetExtension(file)?.ToLower()))
-                                      .Select(filePath => new ImageInfo
-                                      {
-                                          FileName = Path.GetFileNameWithoutExtension(filePath),
-                                          FilePath = $"/images/{product}/" + Path.GetFileName(filePath)
-                                      })
-                                      .OrderBy(i => i.FileName)
-                                      .ToList();
+                    _logger.LogInformation("OnGet.图片路径:{imagesDir}", imagesDir);
 
-                    _logger.LogInformation("OnGet.图片列表:{images}", JsonConvert.SerializeObject(Images));
+                    if (Directory.Exists(imagesDir))
+                    {
 
-                    memoryCache.Set(key, images);
+                        images = Directory.EnumerateFiles(imagesDir)
+                                          .Where(file => sourceArray.Contains(Path.GetExtension(file)?.ToLower()))
+                                          .Select(filePath => new ImageInfo
+                                          {
+                                              FileName = Path.GetFileNameWithoutExtension(filePath),
+                                              FilePath = $"/images/{product}/" + Path.GetFileName(filePath)
+                                          })
+                                          .OrderBy(i => i.FileName)
+                                          .ToList();
+
+                        _logger.LogInformation("OnGet.图片列表:{images}", JsonConvert.SerializeObject(images));
+
+                        memoryCache.Set(key, images);
+                    }
                 }
+
+                return images!;
             }
-            return images!;
+            catch (Exception e)
+            {
+                _logger.LogError("GetImages:{message}\r\n{StackTrace}", e.Message, e.StackTrace);
+            }
+
+            return [];
         }
 
     }
