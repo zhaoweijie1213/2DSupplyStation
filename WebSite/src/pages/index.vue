ee5d25e540d22418a8842b810af913f8399a5349
zhaoweijie1213 <zhaoweijie1108@qq.com>
2024-09-28 18:45:55 +0800

    添加网站

---
+++ WebSite/src/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Application Bar</v-app-bar-title>
+  </v-app-bar>
+  <v-bottom-navigation>
+    <v-btn value="recent">
+      <v-icon>mdi-history</v-icon>
+
+      <span>鸣潮</span>
+    </v-btn>
+
+    <v-btn value="favorites">
+      <v-icon>mdi-heart</v-icon>
+
+      <span>崩坏:星穹铁道</span>
+    </v-btn>
+
+    <v-btn value="nearby">
+      <v-icon>mdi-map-marker</v-icon>
+
+      <span>原神</span>
+    </v-btn>
+  </v-bottom-navigation>
+  <HelloWorld />
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { useDisplay } from "vuetify";
+const { mobile } = useDisplay();
+onMounted(() => {
+  console.log(mobile.value); // false
+});
+</script>
