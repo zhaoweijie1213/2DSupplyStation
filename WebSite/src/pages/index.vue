7fc1c2847846cb53c38d4e783ecaa3e0f3231aa6
zhaoweijie <zhaoweijie1108@qq.com>
2024-09-29 12:18:41 +0800

    删除之前的网页

--- WebSite/src/pages/index.vue
+++ WebSite/src/pages/index.vue
@@ -5,16 +5,16 @@
     </template>
     <v-app-bar-title>二次元补给站</v-app-bar-title>
   </v-app-bar>
-  <v-bottom-navigation v-if="mobile" grow>
-    <v-btn value="favorites" @click="clickItem('HomkalStarRail3d')">
+  <v-bottom-navigation v-model="value" v-if="mobile" grow>
+    <v-btn value="HomkalStarRail3d" @click="clickItem('HomkalStarRail3d')">
       <v-icon>mdi-heart</v-icon>
       <span>崩坏:星穹铁道</span>
     </v-btn>
-    <v-btn value="nearby" @click="clickItem('GenshinImpact3d')">
+    <v-btn value="GenshinImpact3d" @click="clickItem('GenshinImpact3d')">
       <v-icon>mdi-map-marker</v-icon>
       <span>原神</span>
     </v-btn>
-    <v-btn value="recent" @click="clickItem('Uncategorlzed')">
+    <v-btn value="Uncategorlzed" @click="clickItem('Uncategorlzed')">
       <v-icon>mdi-history</v-icon>
       <span>不分类</span>
     </v-btn>
@@ -48,6 +48,7 @@ import { ImageInfo, ImagesServiceProxy } from "@/api/api";
 import { useDisplay } from "vuetify";
 const { mobile } = useDisplay();
 const images = ref<ImageInfo[]>([]);
+const value = ref("HomkalStarRail3d");
 onMounted(() => {
   console.log(mobile.value); // false
   clickItem("HomkalStarRail3d");
