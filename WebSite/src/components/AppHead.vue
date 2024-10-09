222de529812a8a2578130568031da330fbfd2723
zhaoweijie <zhaoweijie1108@qq.com>
2024-10-09 14:15:30 +0800

    添加代码

--- WebSite/src/components/AppHead.vue
+++ WebSite/src/components/AppHead.vue
@@ -3,7 +3,7 @@
     <template v-slot:prepend>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </template>
-    <v-app-bar-title>二次元补给站-图库</v-app-bar-title>
+    <v-app-bar-title>图库</v-app-bar-title>
   </v-app-bar>
 </template>
 <script lang="ts" setup>
